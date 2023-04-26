e6ad1f0f9f2b7bf19ed61adfab845569f537e683
Bernardo Costa Balzan <b.balzan@edu.pucrs.br>
2023-04-26 09:20:47 -0300

    Classes implementadas + for:each

--- src/pucrs/myflight/modelo/Geo.java
+++ src/pucrs/myflight/modelo/Geo.java
@@ -3,17 +3,22 @@ package pucrs.myflight.modelo;
 public class Geo {
 	private double latitude;
 	private double longitude;
-	
+
 	public Geo(double latitude, double longitude) {
 		this.latitude = latitude;
 		this.longitude = longitude;
 	}
-	
+
 	public double getLatitude() {
 		return latitude;
 	}
-	
+
 	public double getLongitude() {
 		return longitude;
+	}
+
+	@Override
+	public String toString() {
+		return String.format("Latitude: %.4f ; Longitude: %.4f",latitude, longitude);
 	}
 }
